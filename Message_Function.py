1b481f82d959ab3143592c8018b307921486931e
YU-RU <leo32095@gmail.com>
2020-11-30 01:18:52 +0800

    make it better

--- Message_Function.py
+++ Message_Function.py
@@ -71,49 +71,87 @@ def Food_message():
         )
     )
     return message
-    # message = TemplateSendMessage(
-    #     alt_text='最近美食資訊',
-    #     template=CarouselTemplate(
-    #         columns=[
-    #             CarouselColumn(
-    #                 thumbnail_image_url='https://drive.google.com/file/d/1Lc7TEnZ3liqrlPTQ_vUC2J3vvtK9N69b/view?usp=sharing',
-    #                 title='兄弟蚵仔麵線',
-    #                 text='堅持傳統口味的用心維持了近三十年',
-    #                 actions=[
-    #                     PostbackTemplateAction(
-    #                         label='我喜歡這家店',
-    #                         data='使用者喜歡'
-    #                     ),
-    #                     URITemplateAction(
-    #                         label='100台北市中正區汀州路三段235號',
-    #                         uri='https://goo.gl/maps/FodenKrdkMt7Kq4Z7'
-    #                     ),
-    #                     MessageTemplateAction(
-    #                         label='用戶發送訊息',
-    #                         text='我知道這是1'
-    #                     )
-    #                 ]
-    #             ),
-    #             CarouselColumn(
-    #                 thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
-    #                 title='這是第二塊模板',
-    #                 text='副標題可以自己改',
-    #                 actions=[
-    #                     PostbackTemplateAction(
-    #                         label='回傳一個訊息',
-    #                         data='這是ID=2'
-    #                     ),
-    #                     MessageTemplateAction(
-    #                         label='用戶發送訊息',
-    #                         text='我知道這是2'
-    #                     ),
-    #                     URITemplateAction(
-    #                         label='進入2的網頁',
-    #                         uri='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Number_2_in_light_blue_rounded_square.svg/200px-Number_2_in_light_blue_rounded_square.svg.png'
-    #                     )
-    #                 ]
-    #             )
-    #         ]
-    #     )
-    # )
-    # return message
+
+# ============================================================================================
+
+
+def photoSt_message():
+    message = TemplateSendMessage(
+        alt_text='站姿推薦',
+        template=ImageCarouselTemplate(
+            columns=[
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/uKYgfVs.jpg",
+                    action=URITemplateAction(
+                        label="新鮮水果",
+                        uri="http://img.juimg.com/tuku/yulantu/110709/222-110F91G31375.jpg"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/QOcAvjt.jpg",
+                    action=URITemplateAction(
+                        label="新鮮蔬菜",
+                        uri="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/Np7eFyj.jpg",
+                    action=URITemplateAction(
+                        label="可愛狗狗",
+                        uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/QRIa5Dz.jpg",
+                    action=URITemplateAction(
+                        label="可愛貓咪",
+                        uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
+                    )
+                )
+            ]
+        )
+    )
+    return message
+
+# ============================================================================================
+
+
+def photoSi_message():
+    message = TemplateSendMessage(
+        alt_text='坐姿推薦',
+        template=ImageCarouselTemplate(
+            columns=[
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/uKYgfVs.jpg",
+                    action=URITemplateAction(
+                        label="新鮮水果",
+                        uri="http://img.juimg.com/tuku/yulantu/110709/222-110F91G31375.jpg"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/QOcAvjt.jpg",
+                    action=URITemplateAction(
+                        label="新鮮蔬菜",
+                        uri="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/Np7eFyj.jpg",
+                    action=URITemplateAction(
+                        label="可愛狗狗",
+                        uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/QRIa5Dz.jpg",
+                    action=URITemplateAction(
+                        label="可愛貓咪",
+                        uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
+                    )
+                )
+            ]
+        )
+    )
+    return message
+
+# ============================================================================================
--- app.py
+++ app.py
@@ -51,8 +51,17 @@ def handle_message(event):
         message = Food_message()
         line_bot_api.reply_message(event.reply_token, message)
     elif '拍照' in msg:
-        message = Photo_message()
+        message = TextSendMessage(text='想要什麼姿勢呢?站,坐,其他')
         line_bot_api.reply_message(event.reply_token, message)
+        @handler.add(MessageEvent, message=TextMessage)
+        def handle_message(event):
+            msg = event.message.text
+            if '站' in msg:
+                message = photoSt_message()
+                line_bot_api.reply_message(event.reply_token, message)
+            elif '坐' in msg:
+                message = photoSi_message()
+                line_bot_api.reply_message(event.reply_token, message)
     elif '歷史' in msg:
         message = History_message()
         line_bot_api.reply_message(event.reply_token, message)
