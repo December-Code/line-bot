97ae13b529af2c32d8449ad7c207c878912bebf1
YU-RU <leo32095@gmail.com>
2020-11-30 12:51:08 +0800

    make it better

--- Message_Function.py
+++ Message_Function.py
@@ -30,7 +30,7 @@ def Food_message():
                     ]
                 ),
                 CarouselColumn(
-                    thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
+                    thumbnail_image_url='https://imgur.com/gallery/UMF6RiO',
                     title='這是第二塊模板',
                     text='副標題可以自己改',
                     actions=[
@@ -49,7 +49,7 @@ def Food_message():
                     ]
                 ),
                 CarouselColumn(
-                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png',
+                    thumbnail_image_url='https://imgur.com/gallery/UMF6RiO',
                     title='這是第三個模塊',
                     text='最多可以放十個',
                     actions=[
