28170f1c16eccb9318921b301085e4ce7f63e1ac
YU-RU <leo32095@gmail.com>
2020-11-30 21:42:33 +0800

    make it better

--- Message_Function.py
+++ Message_Function.py
@@ -113,14 +113,28 @@ def photoSt_message():
                 ImageCarouselColumn(
                     image_url="https://i.imgur.com/rpIHO7w.jpg",
                     action=URITemplateAction(
+                        label="自來水廠_破壞水管(原)",
+                        uri="https://i.imgur.com/rpIHO7w.jpg"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/lZSR4Bq.jpg",
+                    action=URITemplateAction(
+                        label="自來水廠_破壞水管(站姿)",
+                        uri="https://i.imgur.com/lZSR4Bq.jpg"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/rpIHO7w.jpg",
+                    action=URITemplateAction(
                         label="寶藏巖_幸福餅乾",
                         uri="https://i.imgur.com/rpIHO7w.jpg"
                     )
                 ),
                 ImageCarouselColumn(
-                    image_url="https://i.imgur.com/xuS9nUe.jpg",
+                    image_url="https://i.imgur.com/lZSR4Bq.jpg",
                     action=URITemplateAction(
-                        label="寶藏巖_幸福餅乾(坐姿)",
+                        label="寶藏巖_幸福餅乾(站姿)",
                         uri="https://i.imgur.com/lZSR4Bq.jpg"
                     )
                 )
