18dfef8587192f115cf835297aba9d7ed46d5216
YU-RU <leo32095@gmail.com>
2020-11-30 21:03:52 +0800

    make it better

--- Message_Function.py
+++ Message_Function.py
@@ -19,9 +19,8 @@ def Food_message():
                             label='我喜歡這家店',
                             data='使用者喜歡'
                         ),
-                        PostbackTemplateAction(
+                        LocationAction(
                             label='大約價錢：50 元',
-                            data=''
                         ),
                         URITemplateAction(
                             label='100台北市中正區汀州路三段235號',
@@ -38,9 +37,8 @@ def Food_message():
                             label='我喜歡這家店',
                             data='使用者喜歡'
                         ),
-                        PostbackTemplateAction(
+                        LocationAction(
                             label='大約價錢：50 元',
-                            data=''
                         ),
                         URITemplateAction(
                             label='100台北市中正區羅斯福路四段52巷16弄4號',
@@ -57,9 +55,8 @@ def Food_message():
                             label='我喜歡這家店',
                             data='使用者喜歡'
                         ),
-                        PostbackTemplateAction(
+                        LocationAction(
                             label='大約價錢：45 元',
-                            data=''
                         ),
                         URITemplateAction(
                             label='100台北市中正區羅斯福路四段108巷2-3號',
@@ -177,34 +174,34 @@ def photola_message():
         alt_text='坐姿推薦',
         template=ImageCarouselTemplate(
             columns=[
+                 ImageCarouselColumn(
+                     image_url="https://i.imgur.com/4PZvAlV.jpg",
+                     action=TemplateAction(
+                         label="自來水廠_管中世界",
+                         # uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
+                     )
+                 ),
                 ImageCarouselColumn(
-                    image_url="https://i.imgur.com/uKYgfVs.jpg",
-                    action=URITemplateAction(
-                        label="新鮮水果",
-                        uri="http://img.juimg.com/tuku/yulantu/110709/222-110F91G31375.jpg"
+                    image_url="https://i.imgur.com/RkrmZjH.jpg",
+                    action=TemplateAction(
+                        label="自來水廠_管中世界('躺姿')",
+                        # uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
                     )
-                ),
-                ImageCarouselColumn(
-                    image_url="https://i.imgur.com/QOcAvjt.jpg",
-                    action=URITemplateAction(
-                        label="新鮮蔬菜",
-                        uri="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg"
-                    )
-                ),
+                 ),
                 ImageCarouselColumn(
                     image_url="https://i.imgur.com/Np7eFyj.jpg",
                     action=URITemplateAction(
                         label="可愛狗狗",
                         uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
                     )
-                ),
+                 ),
                 ImageCarouselColumn(
                     image_url="https://i.imgur.com/QRIa5Dz.jpg",
                     action=URITemplateAction(
                         label="可愛貓咪",
                         uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
                     )
-                )
+                 )
             ]
         )
     )
