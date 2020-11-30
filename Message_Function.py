8ff4f7b7b575f3d4bee8036236151d4689a50a0a
YU-RU <leo32095@gmail.com>
2020-11-30 21:44:52 +0800

    make it better

--- Message_Function.py
+++ Message_Function.py
@@ -124,20 +124,20 @@ def photoSt_message():
                         uri="https://i.imgur.com/lZSR4Bq.jpg"
                     )
                 ),
-                ImageCarouselColumn(
-                    image_url="https://i.imgur.com/rpIHO7w.jpg",
-                    action=URITemplateAction(
-                        label="寶藏巖_幸福餅乾",
-                        uri="https://i.imgur.com/rpIHO7w.jpg"
-                    )
-                ),
-                ImageCarouselColumn(
-                    image_url="https://i.imgur.com/lZSR4Bq.jpg",
-                    action=URITemplateAction(
-                        label="寶藏巖_幸福餅乾(站姿)",
-                        uri="https://i.imgur.com/lZSR4Bq.jpg"
-                    )
-                )
+                # ImageCarouselColumn(
+                #     image_url="https://i.imgur.com/rpIHO7w.jpg",
+                #     action=URITemplateAction(
+                #         label="寶藏巖_幸福餅乾",
+                #         uri="https://i.imgur.com/rpIHO7w.jpg"
+                #     )
+                # ),
+                # ImageCarouselColumn(
+                #     image_url="https://i.imgur.com/lZSR4Bq.jpg",
+                #     action=URITemplateAction(
+                #         label="寶藏巖_幸福餅乾(站姿)",
+                #         uri="https://i.imgur.com/lZSR4Bq.jpg"
+                #     )
+                # )
             ]
         )
     )
