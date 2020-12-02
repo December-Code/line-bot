27178ff65b659c2a14e7a4a1ca66b7b14f8c6c6a
YU-RU <leo32095@gmail.com>
2020-12-02 14:32:20 +0800

    Send Heart

--- Message_Function.py
+++ Message_Function.py
@@ -531,7 +531,7 @@ def HistoryW_message():
 def Send_Heart():
     message = TextSendMessage(
         text='喜歡$',
-        Emoji=(2, '5ac1bfd5040ab15980c9b435', '215')
+        Emoji=(2,3,'5ac1bfd5040ab15980c9b435', '215')
     )
     return message
     # index = '0',
