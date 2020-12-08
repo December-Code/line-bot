52b8b8475f42b769bd3eb86ee2b2933930db15ab
YU-RU <leo32095@gmail.com>
2020-12-08 12:09:24 +0800

    Rich_menu

--- app.py
+++ app.py
@@ -85,28 +85,28 @@ def handle_leave(event):
     print("很高興為你們服務唷!", event.source)
 
 # ============================RichMenu===============================
-    rich_menu_to_create = RichMenu(
-        size=RichMenuSize(width=2500, height=843),
-        selected=False,
-        name="Nice richmenu",
-        chat_bar_text="Tap here",
-        areas=[
-            RichMenuArea(
-                bounds=RichMenuBounds(
-                    x=0,
-                    y=0,
-                    width=2500,
-                    height=843
-                ),
-                action=URIAction(
-                    label='Go to line.me',
-                    uri='https://line.me'
-                )
-            )
-        ]
-    )
-    rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
-    print(rich_menu_id)
+    # rich_menu_to_create = RichMenu(
+    #     size=RichMenuSize(width=2500, height=843),
+    #     selected=True,
+    #     name="main richmenu",
+    #     chat_bar_text="Tap here",
+    #     areas=[
+    #         RichMenuArea(
+    #             bounds=RichMenuBounds(
+    #                 x=0,
+    #                 y=0,
+    #                 width=2500,
+    #                 height=843
+    #             ),
+    #             action=URIAction(
+    #                 label='Go to line.me',
+    #                 uri='https://line.me'
+    #             )
+    #         )
+    #     ]
+    # )
+    # rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+    # print(rich_menu_id)
 
 
 # 處理訊息
