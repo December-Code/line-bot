59a12009b6bfe6252beec717c21f049fda717f1b
YU-RU <leo32095@gmail.com>
2020-12-22 00:34:01 +0800

    add flex

--- Flex.py
+++ Flex.py
@@ -20,281 +20,221 @@ def FoodW_message():
     # )
     message = FlexSendMessage(
         alt_text='hello',
-        contents={
-            'type': 'bubble',
-            'direction': 'ltr',
-            'hero': {
-                'type': 'image',
-                'url': 'https://example.com/cafe.jpg',
-                'size': 'full',
-                'aspectRatio': '20:13',
-                'aspectMode': 'cover',
-                'action': {'type': 'uri', 'uri': 'http://example.com', 'label': 'label'}
-            }
-        }
+        contents=index
     )
     return message
 
 
-# index = {
-#     "type": "carousel",
-#     "contents": [
-#         {
-#             "type": "bubble",
-#             "size": "micro",
-#             "hero": {
-#                 "type": "image",
-#                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip10.jpg",
-#                 "size": "full",
-#                 "aspectMode": "cover",
-#                 "aspectRatio": "320:213"
-#             },
-#             "body": {
-#                 "type": "box",
-#                 "layout": "vertical",
-#                 "contents": [
-#                     {
-#                         "type": "text",
-#                         "text": "Brown Cafe",
-#                         "weight": "bold",
-#                         "size": "sm",
-#                         "wrap": true
-#                     },
-#                     {
-#                         "type": "box",
-#                         "layout": "baseline",
-#                         "contents": [
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-#                             },
-#                             {
-#                                 "type": "text",
-#                                 "text": "4.0",
-#                                 "size": "xs",
-#                                 "color": "#8c8c8c",
-#                                 "margin": "md",
-#                                 "flex": 0
-#                             }
-#                         ]
-#                     },
-#                     {
-#                         "type": "box",
-#                         "layout": "vertical",
-#                         "contents": [
-#                             {
-#                                 "type": "box",
-#                                 "layout": "baseline",
-#                                 "spacing": "sm",
-#                                 "contents": [
-#                                   {
-#                                       "type": "text",
-#                                       "text": "東京旅行",
-#                                       "wrap": true,
-#                                       "color": "#8c8c8c",
-#                                       "size": "xs",
-#                                       "flex": 5
-#                                   }
-#                                 ]
-#                             }
-#                         ]
-#                     }
-#                 ],
-#                 "spacing": "sm",
-#                 "paddingAll": "13px"
-#             }
-#         },
-#         {
-#             "type": "bubble",
-#             "size": "micro",
-#             "hero": {
-#                 "type": "image",
-#                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip11.jpg",
-#                 "size": "full",
-#                 "aspectMode": "cover",
-#                 "aspectRatio": "320:213"
-#             },
-#             "body": {
-#                 "type": "box",
-#                 "layout": "vertical",
-#                 "contents": [
-#                     {
-#                         "type": "text",
-#                         "text": "Brow&Cony's Restaurant",
-#                         "weight": "bold",
-#                         "size": "sm",
-#                         "wrap": true
-#                     },
-#                     {
-#                         "type": "box",
-#                         "layout": "baseline",
-#                         "contents": [
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-#                             },
-#                             {
-#                                 "type": "text",
-#                                 "text": "4.0",
-#                                 "size": "sm",
-#                                 "color": "#8c8c8c",
-#                                 "margin": "md",
-#                                 "flex": 0
-#                             }
-#                         ]
-#                     },
-#                     {
-#                         "type": "box",
-#                         "layout": "vertical",
-#                         "contents": [
-#                             {
-#                                 "type": "box",
-#                                 "layout": "baseline",
-#                                 "spacing": "sm",
-#                                 "contents": [
-#                                   {
-#                                       "type": "text",
-#                                       "text": "東京旅行",
-#                                       "wrap": true,
-#                                       "color": "#8c8c8c",
-#                                       "size": "xs",
-#                                       "flex": 5
-#                                   }
-#                                 ]
-#                             }
-#                         ]
-#                     }
-#                 ],
-#                 "spacing": "sm",
-#                 "paddingAll": "13px"
-#             }
-#         },
-#         {
-#             "type": "bubble",
-#             "size": "micro",
-#             "hero": {
-#                 "type": "image",
-#                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip12.jpg",
-#                 "size": "full",
-#                 "aspectMode": "cover",
-#                 "aspectRatio": "320:213"
-#             },
-#             "body": {
-#                 "type": "box",
-#                 "layout": "vertical",
-#                 "contents": [
-#                     {
-#                         "type": "text",
-#                         "text": "Tata",
-#                         "weight": "bold",
-#                         "size": "sm"
-#                     },
-#                     {
-#                         "type": "box",
-#                         "layout": "baseline",
-#                         "contents": [
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-#                             },
-#                             {
-#                                 "type": "icon",
-#                                 "size": "xs",
-#                                 "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-#                             },
-#                             {
-#                                 "type": "text",
-#                                 "text": "4.0",
-#                                 "size": "sm",
-#                                 "color": "#8c8c8c",
-#                                 "margin": "md",
-#                                 "flex": 0
-#                             }
-#                         ]
-#                     },
-#                     {
-#                         "type": "box",
-#                         "layout": "vertical",
-#                         "contents": [
-#                             {
-#                                 "type": "box",
-#                                 "layout": "baseline",
-#                                 "spacing": "sm",
-#                                 "contents": [
-#                                   {
-#                                       "type": "text",
-#                                       "text": "東京旅行",
-#                                       "wrap": true,
-#                                       "color": "#8c8c8c",
-#                                       "size": "xs",
-#                                       "flex": 5
-#                                   }
-#                                 ]
-#                             }
-#                         ]
-#                     }
-#                 ],
-#                 "spacing": "sm",
-#                 "paddingAll": "13px"
-#             }
-#         }
-#     ]
-# }
+index = {
+    "type": "bubble",
+    "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "text",
+                "text": "RECEIPT",
+                "weight": "bold",
+                "color": "#1DB446",
+                "size": "sm"
+            },
+            {
+                "type": "text",
+                "text": "Brown Store",
+                "weight": "bold",
+                "size": "xxl",
+                "margin": "md"
+            },
+            {
+                "type": "text",
+                "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                "size": "xs",
+                "color": "#aaaaaa",
+                "wrap": True
+            },
+            {
+                "type": "separator",
+                "margin": "xxl"
+            },
+            {
+                "type": "box",
+                "layout": "vertical",
+                "margin": "xxl",
+                "spacing": "sm",
+                "contents": [
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "Energy Drink",
+                                "size": "sm",
+                                "color": "#555555",
+                                "flex": 0
+                            },
+                            {
+                                "type": "text",
+                                "text": "$2.99",
+                                "size": "sm",
+                                "color": "#111111",
+                                "align": "end"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "Chewing Gum",
+                                "size": "sm",
+                                "color": "#555555",
+                                "flex": 0
+                            },
+                            {
+                                "type": "text",
+                                "text": "$0.99",
+                                "size": "sm",
+                                "color": "#111111",
+                                "align": "end"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "Bottled Water",
+                                "size": "sm",
+                                "color": "#555555",
+                                "flex": 0
+                            },
+                            {
+                                "type": "text",
+                                "text": "$3.33",
+                                "size": "sm",
+                                "color": "#111111",
+                                "align": "end"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "separator",
+                        "margin": "xxl"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "margin": "xxl",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "ITEMS",
+                                "size": "sm",
+                                "color": "#555555"
+                            },
+                            {
+                                "type": "text",
+                                "text": "3",
+                                "size": "sm",
+                                "color": "#111111",
+                                "align": "end"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "TOTAL",
+                                "size": "sm",
+                                "color": "#555555"
+                            },
+                            {
+                                "type": "text",
+                                "text": "$7.31",
+                                "size": "sm",
+                                "color": "#111111",
+                                "align": "end"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "CASH",
+                                "size": "sm",
+                                "color": "#555555"
+                            },
+                            {
+                                "type": "text",
+                                "text": "$8.0",
+                                "size": "sm",
+                                "color": "#111111",
+                                "align": "end"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "CHANGE",
+                                "size": "sm",
+                                "color": "#555555"
+                            },
+                            {
+                                "type": "text",
+                                "text": "$0.69",
+                                "size": "sm",
+                                "color": "#111111",
+                                "align": "end"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "type": "separator",
+                "margin": "xxl"
+            },
+            {
+                "type": "box",
+                "layout": "horizontal",
+                "margin": "md",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "PAYMENT ID",
+                        "size": "xs",
+                        "color": "#aaaaaa",
+                        "flex": 0
+                    },
+                    {
+                        "type": "text",
+                        "text": "#743289384279",
+                        "color": "#aaaaaa",
+                        "size": "xs",
+                        "align": "end"
+                    }
+                ]
+            }
+        ]
+    },
+    "styles": {
+        "footer": {
+            "separator": True
+        }
+    }
+}
