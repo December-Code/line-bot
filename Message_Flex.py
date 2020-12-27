b5892569e3089c2ebff6ad6072f2b6255f2c9b3f
December-Code <leo32095@gmail.com>
2020-12-27 19:24:07 +0800

    add flex food

--- Message_Flex.py
+++ Message_Flex.py
@@ -177,170 +177,170 @@ noodle = {
 # ===========================鴉片粉圓================================
 opiumIce = {
     "type": "bubble",
-            "hero": {
-                "type": "image",
-                "url": "https://i.imgur.com/HOyJCWN.jpg",
-                "size": "full",
-                "aspectRatio": "20:13",
-                "aspectMode": "cover",
-                "action": {
-                    "type": "uri",
-                    "label": "Line",
-                    "uri": "https://linecorp.com/"
-                }
-            },
+    "hero": {
+        "type": "image",
+        "url": "https://i.imgur.com/HOyJCWN.jpg",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "action": {
+            "type": "uri",
+            "label": "Line",
+            "uri": "https://linecorp.com/"
+        }
+    },
     "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "text",
+                "text": "鴉片粉圓",
+                "weight": "bold",
+                "size": "xl",
+                "contents": []
+            },
+            {
                 "type": "box",
-                "layout": "vertical",
+                "layout": "baseline",
+                "margin": "md",
                 "contents": [
                     {
+                        "type": "icon",
+                        "url": "https://i.imgur.com/IB5Xh7u.png",
+                        "size": "md"
+                    },
+                    {
                         "type": "text",
-                        "text": "鴉片粉圓",
+                        "text": "234",
                         "weight": "bold",
-                        "size": "xl",
+                        "size": "lg",
+                        "flex": 0,
+                        "margin": "md",
                         "contents": []
+                    },
+                    {
+                        "type": "text",
+                        "text": "人推薦",
+                        "size": "md",
+                        "margin": "sm",
+                        "contents": []
+                    },
+                    {
+                        "type": "icon",
+                        "url": "https://i.imgur.com/baY264N.png",
+                        "size": "xxl",
+                        "position": "relative",
+                        "offsetStart": "12%"
+                    },
+                    {
+                        "type": "text",
+                        "text": "我推薦",
+                        "weight": "regular",
+                        "align": "end",
+                        "action": {
+                            "type": "postback",
+                            "label": "店家推薦",
+                            "text": "喜歡這家店:鴉片粉圓",
+                            "data": "使用者喜歡:鴉片粉圓"
+                        },
+                        "contents": []
+                    }
+                ]
+            },
+            {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "margin": "lg",
+                "contents": [
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "地址",
+                                "weight": "bold",
+                                "size": "md",
+                                "flex": 1,
+                                "align": "start",
+                                "contents": []
+                            },
+                            {
+                                "type": "text",
+                                "text": "台北市 中正區 羅斯福路四段 52巷 16弄 4號",
+                                "weight": "bold",
+                                "size": "md",
+                                "flex": 5,
+                                "align": "start",
+                                "wrap": True,
+                                "contents": []
+                            }
+                        ]
                     },
                     {
                         "type": "box",
                         "layout": "baseline",
-                        "margin": "md",
-                        "contents": [
-                            {
-                                "type": "icon",
-                                "url": "https://i.imgur.com/IB5Xh7u.png",
-                                "size": "md"
-                            },
-                            {
-                                "type": "text",
-                                "text": "234",
-                                "weight": "bold",
-                                "size": "lg",
-                                "flex": 0,
-                                "margin": "md",
-                                "contents": []
-                            },
-                            {
-                                "type": "text",
-                                "text": "人推薦",
-                                "size": "md",
-                                "margin": "sm",
-                                "contents": []
-                            },
-                            {
-                                "type": "icon",
-                                "url": "https://i.imgur.com/baY264N.png",
-                                "size": "xxl",
-                                "position": "relative",
-                                "offsetStart": "12%"
-                            },
-                            {
-                                "type": "text",
-                                "text": "我推薦",
-                                "weight": "regular",
-                                "align": "end",
-                                "action": {
-                                    "type": "postback",
-                                    "label": "店家推薦",
-                                    "text": "喜歡這家店:鴉片粉圓",
-                                    "data": "使用者喜歡:鴉片粉圓"
-                                },
-                                "contents": []
-                            }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "vertical",
                         "spacing": "sm",
-                        "margin": "lg",
                         "contents": [
                             {
-                                "type": "box",
-                                "layout": "baseline",
-                                "spacing": "sm",
-                                "contents": [
-                                  {
-                                      "type": "text",
-                                      "text": "地址",
-                                      "weight": "bold",
-                                      "size": "md",
-                                      "flex": 1,
-                                      "align": "start",
-                                      "contents": []
-                                  },
-                                    {
-                                      "type": "text",
-                                      "text": "台北市 中正區 羅斯福路四段 52巷16弄4號",
-                                      "weight": "bold",
-                                      "size": "md",
-                                      "flex": 5,
-                                      "align": "start",
-                                      "wrap": True,
-                                      "contents": []
-                                  }
-                                ]
+                                "type": "text",
+                                "text": "時間",
+                                "weight": "bold",
+                                "size": "md",
+                                "flex": 1,
+                                "contents": []
                             },
                             {
-                                "type": "box",
-                                "layout": "baseline",
-                                "spacing": "sm",
-                                "contents": [
-                                    {
-                                        "type": "text",
-                                        "text": "時間",
-                                        "weight": "bold",
-                                        "size": "md",
-                                        "flex": 1,
-                                        "contents": []
-                                    },
-                                    {
-                                        "type": "text",
-                                        "text": "12:00 ~ 23:30",
-                                        "weight": "bold",
-                                        "size": "md",
-                                        "flex": 5,
-                                        "wrap": True,
-                                        "contents": []
-                                    }
-                                ]
+                                "type": "text",
+                                "text": "12:00 ~ 23:30",
+                                "weight": "bold",
+                                "size": "md",
+                                "flex": 5,
+                                "wrap": True,
+                                "contents": []
                             }
                         ]
-                    }
-                ]
-            },
-    "footer": {
-                "type": "box",
-                "layout": "vertical",
-                "flex": 0,
-                "spacing": "sm",
-                "contents": [
-                    {
-                        "type": "button",
-                        "action": {
-                          "type": "message",
-                          "label": "菜單",
-                          "text": "想知道價目表:鴉片粉圓"
-                        },
-                        "height": "sm",
-                        "style": "primary"
-                    },
-                    {
-                        "type": "button",
-                        "action": {
-                            "type": "message",
-                            "label": "我想了解更多~",
-                            "text": "我想了解更多:鴉片粉圓"
-                        },
-                        "height": "sm",
-                        "style": "secondary"
-                    },
-                    {
-                        "type": "spacer",
-                        "size": "sm"
                     }
                 ]
             }
-},
+        ]
+    },
+    "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "flex": 0,
+        "spacing": "sm",
+        "contents": [
+            {
+                "type": "button",
+                "action": {
+                    "type": "message",
+                    "label": "菜單",
+                    "text": "想知道價目表:鴉片粉圓"
+                },
+                "height": "sm",
+                "style": "primary"
+            },
+            {
+                "type": "button",
+                "action": {
+                    "type": "message",
+                    "label": "我想了解更多~",
+                    "text": "我想了解更多:鴉片粉圓"
+                },
+                "height": "sm",
+                "style": "secondary"
+            },
+            {
+                "type": "spacer",
+                "size": "sm"
+            }
+        ]
+    }
+}
 
 
 # ===========================劉記蛋餅================================
